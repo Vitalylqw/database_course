13e84f1c453e103ae964a845d7b0b70e29413f15
Vitalylqw <vitalylqw@gmail.com>
2020-01-16 17:58:30 +0300

    4 work

---
+++ less_5/hw_4.sql
@@ -0,0 +1,12 @@
+--»з таблицы users необходимо извлечь пользователей, родившихс€ в августе и мае.
+--ћес€цы заданы в виде списка английских названий ('may', 'august')
+
+SELECT 
+name »м€,
+CASE 
+WHEN  month(birthday_at)=5 THEN 'may'
+WHEN  month(birthday_at) = 8 THEN 'august'
+END AS  'ћес€ц рождени€'
+FROM users where  month(birthday_at)=5 or month(birthday_at) = 8;
+
+
